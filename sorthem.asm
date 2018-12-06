119ea836fa3a2361eb359bba111c31f0c18bba1d
Shahbaaz Shakil <shahbaaz.shakil@gmail.com>
2018-12-06 05:48:44 -0500

    sorthem: subprogram sorthem expects two parameters on the stack, sorts the disks on the peg in descending order recursively

--- sorthem.asm
+++ sorthem.asm
@@ -5,6 +5,7 @@ extern rconf
 section .data
 errorMsg: dd "Invalid argument. sorthem takes exactly 1 argument. Argument must be >= 2 and <= 9.",10,0
 initMsg: dd "initial configuration",10,0
+finishMsg: dd "final configuration",10,0
 line1:	db "          o|o          ",10,0
 line2:	db "         oo|oo         ",10,0
 line3:	db "        ooo|ooo        ",10,0
@@ -17,6 +18,8 @@ line9:	db "  ooooooooo|ooooooooo  ",10,0
 line10:	db "XXXXXXXXXXXXXXXXXXXXXXX",10,0
 maxPegs: dd 0
 pegArray: times 10 dd 0
+
+section .bss
 
 section .text
 global asm_main
@@ -107,6 +110,62 @@ nextLine:
 	call read_char
 	jmp endProgram
 
+sorthem:
+	mov ecx, [esp+4]
+	mov edx, [esp+8]
+	push ecx
+	push edx
+	mov eax, [maxPegs]
+	push eax
+	push pegArray
+	call showp
+	add esp, 8
+	pop edx
+	pop ecx
+	call sorthemRec
+	ret
+
+sorthemRec:
+	cmp edx, dword 2
+	je sorthemSwap
+	push edx
+	push ecx
+	add ecx, 4
+	dec edx
+	call sorthemRec
+	pop ecx
+	pop edx
+
+sorthemSwap:
+	mov eax, [ecx]
+	cmp eax, [ecx+4]
+	jna sorthemCont
+
+sorthemEnd:
+	push ecx
+	push edx
+	mov eax, [maxPegs]
+	push eax
+	push pegArray
+	call showp
+	add esp, 8
+	pop edx
+	pop ecx
+	ret
+
+sorthemCont:
+	mov eax, [ecx]
+	mov edi, ecx
+	add edi, 4
+	mov ebx, [edi]
+	mov [ecx], ebx
+	mov [edi], eax
+	add ecx, 4
+	dec edx
+	cmp edx, dword 1
+	ja sorthemSwap
+	jmp sorthemEnd
+
 asm_main:
 	enter 0,0
 	pusha
@@ -129,6 +188,7 @@ asm_main:
 	mov eax, 0
 	mov al, bl
 	sub al, 48
+	mov [maxPegs], eax
 
 	push eax
 	push pegArray
@@ -139,6 +199,29 @@ asm_main:
 	call print_string
 	call print_nl
 	push maxPegs
+	push pegArray
+	call showp
+	add esp, 8
+
+	mov eax, [ebp+8]
+	mov ebx, [ebp+12]
+	mov edx, [ebx+4]
+	mov al, byte [edx]
+	sub eax, dword '0'
+	mov [maxPegs], eax
+
+	mov eax, [maxPegs]
+	push eax
+	push pegArray
+	call sorthem
+	add esp, 8
+
+	mov eax, finishMsg
+	call print_string
+	call print_nl
+
+	mov eax, [maxPegs]
+	push eax
 	push pegArray
 	call showp
 	add esp, 8
